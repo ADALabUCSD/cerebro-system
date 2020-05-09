ff271f0b5300e4599802741356c3892ab965c546
scnakandala <supun.nakandala@gmail.com>
2020-05-08 21:05:52 -0700

    removing redundant import

--- docs-src/source/conf.py
+++ docs-src/source/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-from recommonmark.parser import CommonMarkParser
-
 # -- Project information -----------------------------------------------------
 
 project = 'Cerebro'
