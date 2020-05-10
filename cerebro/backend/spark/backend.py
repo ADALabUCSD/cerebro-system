ee5c25b2c7b4f47941513a254ad8153e945152aa
scnakandala <supun.nakandala@gmail.com>
2020-05-09 21:14:22 -0700

    disabling disk cache

--- cerebro/backend/spark/backend.py
+++ cerebro/backend/spark/backend.py
@@ -307,6 +307,7 @@ def _data_readers_fn(remote_store, shard_count, schema_fields, avg_row_size, cac
                                          shard_count=shard_count,
                                          hdfs_driver=PETASTORM_HDFS_DRIVER,
                                          schema_fields=schema_fields,
+                                         workers_count=10,
                                          cache_type='local-disk',
                                          cache_size_limit=cache_size_limit,
                                          cache_row_size_estimate=avg_row_size)
@@ -317,6 +318,7 @@ def _data_readers_fn(remote_store, shard_count, schema_fields, avg_row_size, cac
                                            shard_count=shard_count,
                                            hdfs_driver=PETASTORM_HDFS_DRIVER,
                                            schema_fields=schema_fields,
+                                           workers_count=10,
                                            cache_type='local-disk',
                                            cache_size_limit=cache_size_limit,
                                            cache_row_size_estimate=avg_row_size)
