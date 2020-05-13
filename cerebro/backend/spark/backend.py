f2564722224973f958cacf88af5542051b3d9bf2
scnakandala <supun.nakandala@gmail.com>
2020-05-12 22:48:22 -0700

    specifying the total number of data partitions

--- cerebro/backend/spark/backend.py
+++ cerebro/backend/spark/backend.py
@@ -257,7 +257,7 @@ class SparkBackend(Backend):
         return util.get_simple_meta_from_parquet(store, label_columns + feature_columns)
 
     def prepare_data(self, store, dataset, validation, label_columns=['label'], feature_columns=['features'],
-                     partitions_per_worker=10, parquet_row_group_size_mb=8, dataset_idx=None):
+                     num_partitions=None, parquet_row_group_size_mb=8, dataset_idx=None):
         """
         Prepare data by writing out into persistent storage
 
@@ -266,12 +266,13 @@ class SparkBackend(Backend):
         :param validation: Fraction of validation data (e.g., 0.25) or name of the DataFrame column indicating validation.
         :param label_columns: List of label/output columns (default=['label']).
         :param feature_columns: List of feature columns (default=['features']).
-        :param partitions_per_worker: Number of data partitions per worker.
+        :param num_partitions: Number of data partitions of the output. If None, will default to the current number of
+         input dataset partitions.
         :param parquet_row_group_size_mb: Parquet row group size in MBs (default 8 MB) .
         :param dataset_idx: Dataset index if storing multiple datasets in the same directory.
         """
         return util.prepare_data(self._num_workers(), store, dataset, label_columns, feature_columns, validation,
-                                 partitions_per_worker=partitions_per_worker, dataset_idx=dataset_idx, verbose=self.settings.verbose)
+                                 num_partitions=dataset.rdd.getNumPartitions(), dataset_idx=dataset_idx, verbose=self.settings.verbose)
 
     def _num_workers(self):
         """
