59cc68e5fdab9edac233ff22d3847306c55f280f
Zijian Jim <zijianjim@outlook.com>
2021-12-01 15:08:08 -0800

    remain only five commits

---
+++ keepfive.sh
@@ -0,0 +1,1 @@
+git config --global alias.rebase-last-five '!b="$(git rev-parse --abbrev-ref HEAD)" ; h="$(git rev-parse $b)" ; echo "Current branch: $b $h" ; c="$(git rev-parse $b~4)" ; echo "Recreating $b branch with initial commit $c ..." ; git checkout --orphan new-start $c ; git commit -C $c ; git rebase --onto new-start $c $b ; git branch -d new-start ; git gc'
